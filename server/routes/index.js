27b097b34737701d5ae6e531b09ea009f41d97e1
Kushagra Jain <kushjain1998@gmail.com>
2023-01-15 00:33:33 +0530

    Server routes added

---
+++ server/routes/index.js
@@ -0,0 +1,17 @@
+const applyRoutes = (app) => {
+    app.get('/', (req,res)=>res.send("API is working fine"));
+
+    app.post('/user', (req,res)=>res.send("create-user"));
+
+    app.post('/login', (req,res)=>res.send("login"));
+
+    app.post('/channel', (req,res)=>res.send("channel"));
+
+    app.get('/search-user', (req,res)=>res.send("search-user"));
+
+    app.get('/channel-list', (req,res)=>res.send("channel-list"));
+
+    app.post('/message', (req,res)=>res.send("message"));
+
+};
+export default applyRoutes;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 import APP from "express";
 import connectDB from "./dbConnection";
+import configureExpressApp from "./config";
+import applyRoutes from "./routes"
 
 const app = new APP();
+configureExpressApp(app);
 
 const PORT = 3001;
 
@@ -9,7 +12,8 @@ const startServer = () => {
     console.log("DB trying to connect on " + new Date());
     Promise.all([connectDB()]).then(()=>{
         app.listen(PORT);
-        console.log(`Server started on port ${PORT}`)
+        console.log(`Server started on port ${PORT}`);
+        applyRoutes(app);
     })
 };
 
